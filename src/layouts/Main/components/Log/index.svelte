4a2cc3a40aa368d4097fd706b6481d1026a3f6a6
Philipp Babkin <jazz@iMac>
2024-08-22 06:11:25 +0300

    ...

--- src/layouts/Main/components/Log/index.svelte
+++ src/layouts/Main/components/Log/index.svelte
@@ -9,10 +9,16 @@
 
     import { Entity, collector } from '@/helpers/entity';
 
+    import { Avatar } from '@/components';
+
     import {
 		notificationView,
         notificationViewAll,
 	} from '@/queries/notification';
+
+    import {
+        eventConnectionResponse
+    } from '@/queries/event';
 
 
     // svelte-ignore unused-export-let
@@ -41,11 +47,26 @@
 	});
 
 
+    /* DATA: eventConnectionResponseHandler */
+	const eventConnectionResponseHandler = new Entity({
+		model: eventConnectionResponse.model,
+		retriever: eventConnectionResponse.retriever,
+        /*
+        onSuccess: () => {
+            if (data.onRefresh)
+                data.onRefresh();
+        },
+        */
+	});
+
+
     let data: {
         notifications: { [key: string]: any }[],
+        connections: { [key: string]: any },
         onRefresh: any,
     } = {
         notifications: [],
+        connections: {},
         onRefresh: null,
     };
 
@@ -76,6 +97,22 @@
             [ 
                 notificationViewAllHandler,
                 {}
+            ],
+        ]);
+    }
+
+
+    /* sendResponse */
+    function sendResponse(t, eventId, userId, resp) {
+        collector.get([
+            [ 
+                eventConnectionResponseHandler,
+                {
+                    timeNotify: t,
+                    eventId: eventId,
+                    userId: userId,
+                    resp: resp,
+                }
             ],
         ]);
     }
@@ -119,16 +156,154 @@
             <div
                 class="w-full px-3 mt-3"
             >
-                {#if n.event == 'connection'}
+
+
+
+                {#if n.event == 'connection_add'}
                     <div class="rounded-2xl w-full overflow-hidden p-3 bg-base-200">
                         <div
                             class="text-xs w-full mb-1"
                             class:opacity-50="{n.time_view}"
                         >{toFeedDate(n.time_notify)}</div>
+                        <div class="w-full flex items-start">
+                            <button
+                                on:click="{() => {
+                                    view(n.time_notify_key);
+                                    router.go('/residents/' + n.data.initiator.id.toString());
+                                    logHide();
+                                }}"
+                                class="w-8 h-8 shrink-0 grow-0 mt-0.5 mr-2.5"
+                                class:opacity-50="{n.time_view}"
+                            >
+                                <Avatar
+                                    user="{{
+                                        id: n.data.initiator.id,
+                                        name: n.data.initiator.name,
+                                        avatar_hash: n.data.initiator.hash,
+                                        roles: [ 'client' ],
+                                        telegram: '',
+                                    }}"
+                                />
+                                </button>
+                            <div
+                                class="text-sm w-full shrink-1 grow-1"
+                                class:opacity-50="{n.time_view}"
+                            ><button
+                                on:click="{() => {
+                                    view(n.time_notify_key);
+                                    router.go('/residents/' + n.data.initiator.id.toString());
+                                    logHide();
+                                }}"
+                                class="font-semibold text-moderate"
+                            >{nameNormalization(n.data.initiator.name, 2)}</button> предложил Вам личную встречу на мероприятии <span class="font-semibold text-moderate">{toDateText(n.data.event.time_event).split(/\s+/)[0]} {toDateText(n.data.event.time_event).split(/\s+/)[1]}</span></div>
+                        </div>
+                        <!--
+                        <div class="flex justify-between mt-2">
+                            <button
+                                class="btn btn-sm bg-base-300 flex shrink-0 grow-0"
+                                on:click="{() => {
+                                    view(n.time_notify_key);
+                                    router.go('/residents/' + n.data.initiator.id.toString());
+                                    logHide();
+                                }}"
+                            >
+                                <span class:opacity-50="{n.time_view}">В профиль</span>
+                                <svg class="w-4 h-4" class:opacity-50="{n.time_view}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32"><path d="M16 4a5 5 0 1 1-5 5a5 5 0 0 1 5-5m0-2a7 7 0 1 0 7 7a7 7 0 0 0-7-7z" fill="currentColor"></path><path d="M26 30h-2v-5a5 5 0 0 0-5-5h-6a5 5 0 0 0-5 5v5H6v-5a7 7 0 0 1 7-7h6a7 7 0 0 1 7 7z" fill="currentColor"></path></svg>
+                            </button>
+                            <button
+                                class="btn btn-sm bg-base-300 flex shrink-0 grow-0"
+                                on:click="{() => {
+                                    view(n.time_notify_key);
+                                    router.go('/events/' + n.data.event.id.toString());
+                                    logHide();
+                                }}"
+                            >
+                                <span class:opacity-50="{n.time_view}">К событию</span>
+                                <svg class="w-4 h-4" class:opacity-50="{n.time_view}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32"><path d="M26 4h-4V2h-2v2h-8V2h-2v2H6c-1.1 0-2 .9-2 2v20c0 1.1.9 2 2 2h20c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 22H6V12h20v14zm0-16H6V6h4v2h2V6h8v2h2V6h4v4z" fill="currentColor"></path></svg>
+                            </button>
+                        </div>
+                        -->
+                        {#if
+                            !data.connections[n.data.event.id.toString()] ||
+                            !data.connections[n.data.event.id.toString()][n.data.initiator.id.toString()] ||
+                            data.connections[n.data.event.id.toString()][n.data.initiator.id.toString()]['response'] === null
+                        }
+                            <div class="flex justify-between items-center mt-2">
+                                <button
+                                    class="btn btn-sm btn-success flex shrink-0 grow-0"
+                                    on:click="{() => {
+                                        sendResponse(n.time_notify_key, n.data.event.id, n.data.initiator.id, true);
+                                    }}"
+                                >
+                                    <span class:opacity-50="{n.time_view}">Согласиться</span>
+                                </button>
+                                <button
+                                    class="btn btn-sm btn-error flex shrink-0 grow-0"
+                                    on:click="{() => {
+                                        sendResponse(n.time_notify_key, n.data.event.id, n.data.initiator.id, false);
+                                    }}"
+                                >
+                                    <span class:opacity-50="{n.time_view}">Отклонить</span>
+                                </button>
+                            </div>
+                        {:else}
+                            <div class="flex justify-between items-center mt-2">
+                                {#if data.connections[n.data.event.id.toString()][n.data.initiator.id.toString()]['response']}
+                                    <div class="text-success font-semibold text-sm">Вы согласились</div>
+                                {:else}
+                                    <div class="text-error font-semibold text-sm">Вы отказались</div>
+                                {/if}
+                                <button
+                                    class="btn btn-sm bg-base-300 flex shrink-0 grow-0"
+                                    on:click="{() => {
+                                        sendResponse(n.time_notify_key, n.data.event.id, n.data.initiator.id, null);
+                                    }}"
+                                >
+                                    <span>Передумать</span>
+                                </button>
+                            </div>
+                        {/if}
+                    </div>
+
+
+                {:else if n.event == 'connection_response'}
+                    <div class="rounded-2xl w-full overflow-hidden p-3 bg-base-200">
                         <div
-                            class="text-sm w-full"
+                            class="text-xs w-full mb-1"
                             class:opacity-50="{n.time_view}"
-                        ><span class="font-semibold text-moderate">{nameNormalization(n.data.initiator.name, 2)}</span> предложил Вам личную встречу на мероприятии <span class="font-semibold text-moderate">{toDateText(n.data.event.time_event).split(/\s+/)[0]} {toDateText(n.data.event.time_event).split(/\s+/)[1]}</span></div>
+                        >{toFeedDate(n.time_notify)}</div>
+                        <div class="w-full flex items-start">
+                            <button
+                                on:click="{() => {
+                                    view(n.time_notify_key);
+                                    router.go('/residents/' + n.data.initiator.id.toString());
+                                    logHide();
+                                }}"
+                                class="w-8 h-8 shrink-0 grow-0 mt-0.5 mr-2.5"
+                                class:opacity-50="{n.time_view}"
+                            >
+                                <Avatar
+                                    user="{{
+                                        id: n.data.initiator.id,
+                                        name: n.data.initiator.name,
+                                        avatar_hash: n.data.initiator.hash,
+                                        roles: [ 'client' ],
+                                        telegram: '',
+                                    }}"
+                                />
+                                </button>
+                            <div
+                                class="text-sm w-full shrink-1 grow-1"
+                                class:opacity-50="{n.time_view}"
+                            ><button
+                                on:click="{() => {
+                                    view(n.time_notify_key);
+                                    router.go('/residents/' + n.data.initiator.id.toString());
+                                    logHide();
+                                }}"
+                                class="font-semibold text-moderate"
+                            >{nameNormalization(n.data.initiator.name, 2)}</button> {#if n.data.response}согласился с Вашим предложением{:else}отказался от Вашего предложения{/if} о личной встрече на мероприятии <span class="font-semibold text-moderate">{toDateText(n.data.event.time_event).split(/\s+/)[0]} {toDateText(n.data.event.time_event).split(/\s+/)[1]}</span></div>
+                        </div>
                         <div class="flex justify-between mt-2">
                             <button
                                 class="btn btn-sm bg-base-300 flex shrink-0 grow-0"
@@ -154,16 +329,47 @@
                             </button>
                         </div>
                     </div>
+
+
+
                 {:else if n.event == 'arrive'}
                     <div class="rounded-2xl w-full overflow-hidden p-3 bg-base-200">
                         <div
                             class="text-xs w-full mb-1"
                             class:opacity-50="{n.time_view}"
                         >{toFeedDate(n.time_notify)}</div>
-                        <div
-                            class="text-sm w-full"
-                            class:opacity-50="{n.time_view}"
-                        ><span class="font-semibold text-moderate">{nameNormalization(n.data.user.name, 2)}</span> прибыл на мероприятие <span class="font-semibold text-moderate">{toDateText(n.data.event.time_event).split(/\s+/)[0]} {toDateText(n.data.event.time_event).split(/\s+/)[1]}</span> (назначена встреча)</div>
+                        <div class="w-full flex items-start">
+                            <button
+                                on:click="{() => {
+                                    view(n.time_notify_key);
+                                    router.go('/residents/' + n.data.user.id.toString());
+                                    logHide();
+                                }}"
+                                class="w-8 h-8 shrink-0 grow-0 mt-0.5 mr-2.5"
+                                class:opacity-50="{n.time_view}"
+                            >
+                                <Avatar
+                                    user="{{
+                                        id: n.data.user.id,
+                                        name: n.data.user.name,
+                                        avatar_hash: n.data.user.hash,
+                                        roles: [ 'client' ],
+                                        telegram: '',
+                                    }}"
+                                />
+                                </button>
+                            <div
+                                class="text-sm w-full shrink-1 grow-1"
+                                class:opacity-50="{n.time_view}"
+                            ><button
+                                on:click="{() => {
+                                    view(n.time_notify_key);
+                                    router.go('/residents/' + n.data.user.id.toString());
+                                    logHide();
+                                }}"
+                                class="font-semibold text-moderate"
+                            >{nameNormalization(n.data.user.name, 2)}</button> прибыл на мероприятие <span class="font-semibold text-moderate">{toDateText(n.data.event.time_event).split(/\s+/)[0]} {toDateText(n.data.event.time_event).split(/\s+/)[1]}</span> (назначена встреча)</div>
+                        </div>
                         <div class="flex justify-between mt-2">
                             <button
                                 class="btn btn-sm bg-base-300 flex shrink-0 grow-0"
@@ -173,7 +379,7 @@
                                     logHide();
                                 }}"
                             >
-                                <span class:opacity-50="{n.time_view}">В профиль</span>
+                                <span>В профиль</span>
                                 <svg class="w-4 h-4" class:opacity-50="{n.time_view}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32"><path d="M16 4a5 5 0 1 1-5 5a5 5 0 0 1 5-5m0-2a7 7 0 1 0 7 7a7 7 0 0 0-7-7z" fill="currentColor"></path><path d="M26 30h-2v-5a5 5 0 0 0-5-5h-6a5 5 0 0 0-5 5v5H6v-5a7 7 0 0 1 7-7h6a7 7 0 0 1 7 7z" fill="currentColor"></path></svg>
                             </button>
                             <button
@@ -184,12 +390,15 @@
                                     logHide();
                                 }}"
                             >
-                                <span class:opacity-50="{n.time_view}">К событию</span>
+                                <span>К событию</span>
                                 <svg class="w-4 h-4" class:opacity-50="{n.time_view}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32"><path d="M26 4h-4V2h-2v2h-8V2h-2v2H6c-1.1 0-2 .9-2 2v20c0 1.1.9 2 2 2h20c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 22H6V12h20v14zm0-16H6V6h4v2h2V6h8v2h2V6h4v4z" fill="currentColor"></path></svg>
                             </button>
                         </div>
                     </div>
                 {/if}
+
+
+
             </div>
         {/each}
     </div>
