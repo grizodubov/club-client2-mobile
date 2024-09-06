53d69ce3b25856137a0062e2259ac7cc08b2dc30
Philipp Babkin <jazz@iMac>
2024-09-06 14:53:15 +0300

    ...

--- src/App.svelte
+++ src/App.svelte
@@ -612,18 +612,22 @@
                             {/if}
                         </div>
                     {/if}
-                    <div class="absolute bottom-[-48px] left-[48px] h-[28px] w-[28px] bg-error rounded-[14px] flex items-center justify-around px-1.5">
-                        <svg class="text-base-100 w-4 h-4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 352 512"><path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28L75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256L9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" fill="currentColor"></path></svg>
-                    </div>
-                    <div
-                        class="absolute bottom-[-48px] right-[48px] h-[28px] bg-success rounded-[14px] flex items-center justify-around px-1.5"
-                        class:w-[28px]="{!card.favorites_stats}"
-                        class:w-[64px]="{card.favorites_stats}"
-                    >
-                        <svg class="text-base-100 w-4 h-4 shrink-0 grow-0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512"><path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69L432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z" fill="currentColor"></path></svg>
-                        {#if card.favorites_stats}
-                            <span class="text-base-100 font-medium text-sm shrink-0 grow-0">{card.favorites_stats}</span>
-                        {/if}
+                    <div class="absolute rounded-[14px] bg-scene h-[28px] bottom-[-48px] left-[48px] right-[48px] flex justify-between items-center">
+                        <div class="h-[28px] w-[28px] bg-error rounded-[14px] flex items-center justify-center px-1.5">
+                            <svg class="text-base-100 w-4 h-4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 352 512"><path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28L75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256L9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" fill="currentColor"></path></svg>
+                        </div>
+                        <div
+                            class="h-[28px] bg-success rounded-[14px] flex items-center px-1.5"
+                            class:w-[28px]="{!card.favorites_stats}"
+                            class:justify-center="{!card.favorites_stats}"
+                            class:w-[64px]="{card.favorites_stats}"
+                            class:justify-around="{card.favorites_stats}"
+                        >
+                            <svg class="text-base-100 w-4 h-4 shrink-0 grow-0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512"><path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69L432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z" fill="currentColor"></path></svg>
+                            {#if card.favorites_stats}
+                                <span class="text-base-100 font-medium text-sm shrink-0 grow-0">{card.favorites_stats}</span>
+                            {/if}
+                        </div>
                     </div>
                 </div>
                 <svelte:fragment slot="swipe-btn">
