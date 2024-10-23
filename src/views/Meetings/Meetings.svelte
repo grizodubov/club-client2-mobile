8cebdd6ecca94b65a03053717a48873cd4f30a25
Philipp Babkin <jazz@iMac>
2024-10-23 13:30:15 +0300

    ...

--- src/views/Meetings/Meetings.svelte
+++ src/views/Meetings/Meetings.svelte
@@ -176,7 +176,7 @@
             <div class="h-full flex flex-col">
                 <div class="w-full flex justify-between items-center h-[64px] mb-3 shrink-0 grow-0">
                     <button
-                        class="flex items-center justify-center basis-3/6 h-[48px] border-b-4 ml-5 transition-all duration-300"
+                        class="flex items-center justify-center basis-3/6 h-[48px] border-b-4 ml-4 transition-all duration-300"
                         class:border-base-100="{archive}"
                         class:border-success="{!archive}"
                         on:click="{() => {
@@ -188,10 +188,10 @@
                         }}"
                     >
                         <svg class="text-success w-5 h-5" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 352 512"><path d="M96.06 454.35c.01 6.29 1.87 12.45 5.36 17.69l17.09 25.69a31.99 31.99 0 0 0 26.64 14.28h61.71a31.99 31.99 0 0 0 26.64-14.28l17.09-25.69a31.989 31.989 0 0 0 5.36-17.69l.04-38.35H96.01l.05 38.35zM0 176c0 44.37 16.45 84.85 43.56 115.78c16.52 18.85 42.36 58.23 52.21 91.45c.04.26.07.52.11.78h160.24c.04-.26.07-.51.11-.78c9.85-33.22 35.69-72.6 52.21-91.45C335.55 260.85 352 220.37 352 176C352 78.61 272.91-.3 175.45 0C73.44.31 0 82.97 0 176zm176-80c-44.11 0-80 35.89-80 80c0 8.84-7.16 16-16 16s-16-7.16-16-16c0-61.76 50.24-112 112-112c8.84 0 16 7.16 16 16s-7.16 16-16 16z" fill="currentColor"></path></svg>
-                        <span class="ml-2.5">Назначенные</span>
+                        <span class="ml-2 text-sm leading-6">Назначенные</span>
                     </button>
                     <button
-                        class="flex items-center justify-center basis-3/6 h-[48px] border-b-4 mx-5 transition-all duration-300"
+                        class="flex items-center justify-center basis-3/6 h-[48px] border-b-4 ml-5 mr-4 transition-all duration-300"
                         class:border-base-100="{!archive}"
                         class:border-warning="{archive}"
                         on:click="{() => {
@@ -203,7 +203,7 @@
                         }}"
                     >
                         <svg class="text-warning w-5 h-5" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512"><path d="M32 448c0 17.7 14.3 32 32 32h384c17.7 0 32-14.3 32-32V160H32v288zm160-212c0-6.6 5.4-12 12-12h104c6.6 0 12 5.4 12 12v8c0 6.6-5.4 12-12 12H204c-6.6 0-12-5.4-12-12v-8zM480 32H32C14.3 32 0 46.3 0 64v48c0 8.8 7.2 16 16 16h480c8.8 0 16-7.2 16-16V64c0-17.7-14.3-32-32-32z" fill="currentColor"></path></svg>
-                        <span class="ml-2.5">Архив{#if archive}<span class="ml-2 font-extralight">( <span class="text-neutral">{amountFiltered}</span> / <span class="text-base-300">{amountTotal}</span> )</span>{/if}</span>
+                        <span class="ml-2 text-sm leading-6">Архив{#if archive}<span class="ml-2 font-extralight">( <span class="text-neutral">{amountFiltered}</span> / <span class="opacity-60">{amountTotal}</span> )</span>{/if}</span>
                     </button>
                 </div>
                 {#if archive}
